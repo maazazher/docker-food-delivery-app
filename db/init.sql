37d07b1652c2030435ba7740b8d154d197668b79
Mohammed Maaz Azhar <maazazherg@gmail.com>
2026-03-08 06:20:53 +0300

    Update init.sql

--- db/init.sql
+++ db/init.sql
@@ -1,17 +1,13 @@
--- Create database
 CREATE DATABASE fooddb;
 
--- Use database
 USE fooddb;
 
--- Create table
 CREATE TABLE foods (
   id INT AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(100),
   price INT
 );
 
--- Insert sample data
 INSERT INTO foods (name, price) VALUES
 ('Burger', 100),
 ('Pizza', 200),
